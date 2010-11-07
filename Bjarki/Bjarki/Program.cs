b2770e986bd0dbdfdf74aae80ee165cc0c5758f6
SuprDewd <suprdewd@gmail.com>
2010-11-07 12:36:25 +0000

    Bjarki D1

---
+++ Bjarki/Bjarki/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bjarki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            D1();
+
+            Console.ReadLine();
+        }
+
+        static void D1()
+        {
+            Console.Write("Nafn: ");
+            string s = Console.ReadLine();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                Console.Write(s[i]);
+                Console.Write(" ");
+            }
+        }
+    }
+}
