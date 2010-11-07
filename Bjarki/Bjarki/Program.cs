c5856d713b9e338bda197b250e7849fbc705c298
SuprDewd <suprdewd@gmail.com>
2010-11-07 13:07:13 +0000

    Bjarki D5

--- Bjarki/Bjarki/Program.cs
+++ Bjarki/Bjarki/Program.cs
@@ -16,47 +16,79 @@ namespace Bjarki
     {
         static void Main(string[] args)
         {
-            D4();
+            D5();
 
             Console.ReadLine();
         }
 
-        static void D1()
+        static void D5()
         {
-            Console.Write("Nafn: ");
-            string s = Console.ReadLine();
+            Console.Write("Hver er talan? ");
+            int tala = Console.ReadLine().ToInt();
 
-            for (int i = 0; i < s.Length; i++)
+            string mal = TalaIMal(tala, false).ToLower().Trim().ReplaceAll("  ", " ");
+            Console.WriteLine(mal);
+        }
+
+        static string[] malEiningar = new string[] { "núll", "einn", "tveir", "þrír", "fjórir", "fimm", "sex", "sjö", "átta", "níu", "tíu", "ellefu", "tólf", "þrettán", "fjórtán", "fimmtán", "sekstán", "sautján", "átján", "nítján" };
+        static string[] malTugir = new string[] { "", "tíu", "tuttugu", "þrjátíu", "fjörtíu", "fimmtíu", "sextíu", "sjötíu", "áttatíu", "nítíu", "hundrað" };
+        static string[] malHundrud = new string[] { "", "eitt", "tvö", "þrjú", "fjögur", "fimm", "sex", "sjö", "átta", "níu", "hundruð" };
+
+        static string TalaIMal(int tala, bool h)
+        {
+            int tugir, ein, hundrud, afg;
+            if ((tala >= 0 && tala <= 19 && !h) || (tala >= 0 && tala <= 9 && h))
             {
-                Console.Write(s[i]);
-                Console.Write(" ");
+                return h ? malHundrud[tala] : malEiningar[tala];
             }
-        }
 
-        static void D1B()
-        {
-            Console.Write("Nafn: ");
-            string s = Console.ReadLine();
+            if (tala < 100)
+            {
+                tugir = tala / 10;
+                ein = tala - (tugir * 10);
 
-            Console.WriteLine(String.Join(" ", s.ToCharArray()));
-        }
-
-        static void D2()
-        {
-            string s = File.ReadAllText("D2.txt");
-
-            int n = 1;
-            s.Split(' ').Select(i => i.ToInt()).ForEach(i =>
+                if (ein == 0)
                 {
-                    Console.Write(n++ + ": " + i + "\t");
+                    return malTugir[tugir];
+                }
+                else
+                {
+                    return malTugir[tugir] + " og " + malEiningar[ein];
+                }
+            }
 
-                    for (int j = 0; j < i; j++)
-                    {
-                        Console.Write("*");
-                    }
+            hundrud = tala / 100;
+            afg = tala % 100;
+            tugir = afg / 10;
+            ein = afg % 10;
 
-                    Console.WriteLine();
-                });
+            string hu = hundrud == 1 ? TalaIMal(hundrud, true) + " hundrað " : TalaIMal(hundrud, true) + " hundruð ";
+
+            if (tugir == 0 && ein == 0)
+            {
+                return hu;
+            }
+
+            if (tugir <= 1)
+            {
+                return hu + " og " + malEiningar[ein + (tugir * 10)];
+            }
+
+            return hu + TalaIMal(ein + (tugir * 10), false);
+        }
+
+        static void D4()
+        {
+            Console.Write("Fyrsta tala? ");
+            int f = Console.ReadLine().ToInt();
+
+            Console.Write("Síðasta tala? ");
+            int s = Console.ReadLine().ToInt();
+
+            Console.Write("Veldi ");
+            int v = Console.ReadLine().ToInt();
+
+            Console.WriteLine("Niðurstaða: " + f.To(s).Sum(i => Math.Pow(i, v)));
         }
 
         static void D3()
@@ -75,18 +107,42 @@ namespace Bjarki
             Console.WriteLine(new String('*', s) + new String('X', x) + new String('C', c));
         }
 
-        static void D4()
+        static void D2()
         {
-            Console.Write("Fyrsta tala? ");
-            int f = Console.ReadLine().ToInt();
+            string s = File.ReadAllText("D2.txt");
 
-            Console.Write("Síðasta tala? ");
-            int s = Console.ReadLine().ToInt();
+            int n = 1;
+            s.Split(' ').Select(i => i.ToInt()).ForEach(i =>
+            {
+                Console.Write(n++ + ": " + i + "\t");
 
-            Console.Write("Veldi ");
-            int v = Console.ReadLine().ToInt();
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
 
-            Console.WriteLine("Niðurstaða: " + f.To(s).Sum(i => Math.Pow(i, v)));
+                Console.WriteLine();
+            });
+        }
+
+        static void D1B()
+        {
+            Console.Write("Nafn: ");
+            string s = Console.ReadLine();
+
+            Console.WriteLine(String.Join(" ", s.ToCharArray()));
+        }
+
+        static void D1()
+        {
+            Console.Write("Nafn: ");
+            string s = Console.ReadLine();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                Console.Write(s[i]);
+                Console.Write(" ");
+            }
         }
     }
 }
