0279f67e129d32990e765371608db8c1250e9435
RagnarBR <johnnyahss@hotmail.com>
2010-12-07 14:39:59 +0000

    rawr

---
+++ RBR/PimpMyText[OG]/PimpMyText/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace PimpMyText
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnPimpMyText = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            //
+            // txtInput
+            //
+            this.txtInput.Location = new System.Drawing.Point(12, 12);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(250, 191);
+            this.txtInput.TabIndex = 0;
+            //
+            // btnPimpMyText
+            //
+            this.btnPimpMyText.Location = new System.Drawing.Point(275, 96);
+            this.btnPimpMyText.Name = "btnPimpMyText";
+            this.btnPimpMyText.Size = new System.Drawing.Size(92, 23);
+            this.btnPimpMyText.TabIndex = 1;
+            this.btnPimpMyText.Text = "Pimp My Text";
+            this.btnPimpMyText.UseVisualStyleBackColor = true;
+            this.btnPimpMyText.Click += new System.EventHandler(this.ButtonPimpMyTextClick);
+            //
+            // txtOutput
+            //
+            this.txtOutput.Location = new System.Drawing.Point(378, 12);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(242, 191);
+            this.txtOutput.TabIndex = 0;
+            //
+            // Form1
+            //
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 219);
+            this.Controls.Add(this.btnPimpMyText);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion Windows Form Designer generated code
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnPimpMyText;
+        private System.Windows.Forms.TextBox txtOutput;
+    }
+}
---
+++ RBR/PimpMyText[OG]/PimpMyText/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace PimpMyText
+{
+    public partial class Form1 : Form
+    {
+<<<<<<< HEAD
+        string[] colorStart = new string[] { @"[color=red]", @"[color=blue]", @"[color=cyan]", @"[color=green]", @"[color=black]", @"[color=pink]", @"[color=yellow]", @"[color=purple]"};
+        string colorEnd = @"[/color]";
+        string temp = null;
+        Random rand = new Random();
+        int currentColor = 0, previousColor = 100;
+
+=======
+>>>>>>> 2e7af0ee34f3f638b4693d6a0c44c511bdf6c428
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private const string _Open = "[color={0}]";
+        private const string _Close = "[/color]";
+        private readonly string[] _Colors = new[] { "red", "blue", "cyan", "green", "black", "yellow", "pink", "purple" };
+        private string _LastColor;
+        private readonly Random Rand = new Random();
+
+        private void ButtonPimpMyTextClick(object sender, EventArgs e)
+        {
+            const string s = _Open + "{1}" + _Close;
+            this.txtOutput.Text = String.Join("", this.txtInput.Text.Select(c => Char.IsWhiteSpace(c) ? c : String.Format(s, NewColor(), c)).ToArray());
+        }
+
+        private string NewColor()
+        {
+            string n;
+
+            do
+            {
+<<<<<<< HEAD
+                if (textBox1.Text[i] != ' ')
+                {
+                    for (int x = 0; x < textBox1.Text.Length; x++) //x = 0
+                    {
+                        if (x == i) //x = i
+                        {
+                            NextColor(); 
+                            temp += colorStart[currentColor] + textBox1.Text[x] + colorEnd; //[color=red] + textBox1.Text[0] + [/color]
+                        }
+                    }
+                }
+
+                else
+                {
+                    temp += ' '; 
+                }
+=======
+                n = this._Colors[Rand.Next(0, this._Colors.Length)];
+>>>>>>> 2e7af0ee34f3f638b4693d6a0c44c511bdf6c428
+            }
+            while (n == _LastColor);
+
+            _LastColor = n;
+            return n;
+        }
+
+        public void NextColor()
+        {
+            bool useColor = false;
+
+            while (useColor == false)
+            {
+                currentColor = rand.Next(0, (colorStart.Length - 1));
+
+                if (currentColor == previousColor)
+                {
+                    useColor = false;
+                }
+
+                else
+                {
+                    useColor = true;
+                }
+            }
+            previousColor = currentColor;
+        }
+    }
+}
