c9305e3a2d411b0001cbcbb7ac19a847db94d6fc
RagnarBR <johnnyahss@hotmail.com>
2010-12-07 14:34:18 +0000

    rawr

--- RBR/PimpMyText/PimpMyText/Form1.cs
+++ RBR/PimpMyText/PimpMyText/Form1.cs
@@ -11,9 +11,11 @@ namespace PimpMyText
 {
     public partial class Form1 : Form
     {
-        string[] colorStart = new string[] { @"[color=red]", @"[color=blue]", @"[color=cyan]", @"[color=green]", @"[color=black]", @"[color=pink]", @"[color=yellow]", @"[color=purple]", @"[color=pink]"};
+        string[] colorStart = new string[] { @"[color=red]", @"[color=blue]", @"[color=cyan]", @"[color=green]", @"[color=black]", @"[color=pink]", @"[color=yellow]", @"[color=purple]"};
         string colorEnd = @"[/color]";
         string temp = null;
+        Random rand = new Random();
+        int currentColor = 0, previousColor = 100;
 
         public Form1()
         {
@@ -27,24 +29,46 @@ namespace PimpMyText
 
             for (int i = 0; i < textBox1.Text.Length; i++)
             {
-                if (char.IsLetterOrDigit(textBox1.Text[i])) //r i = 0 
+                if (textBox1.Text[i] != ' ')
                 {
                     for (int x = 0; x < textBox1.Text.Length; x++) //x = 0
                     {
-                        if (x != i) 
+                        if (x == i) //x = i
                         {
-                            temp += textBox1.Text[x]; 
-                        }
-
-                        else //x = i
-                        {
-                            temp += colorStart[0] + textBox1.Text[x] + colorEnd; //[color=red] + textBox1.Text[0] + [/color]
+                            NextColor(); 
+                            temp += colorStart[currentColor] + textBox1.Text[x] + colorEnd; //[color=red] + textBox1.Text[0] + [/color]
                         }
                     }
+                }
+
+                else
+                {
+                    temp += ' '; 
                 }
             }
 
             textBox1.Text = temp;
+        }
+
+        public void NextColor()
+        {
+            bool useColor = false;
+
+            while (useColor == false)
+            {
+                currentColor = rand.Next(0, (colorStart.Length - 1));
+
+                if (currentColor == previousColor)
+                {
+                    useColor = false;
+                }
+
+                else
+                {
+                    useColor = true;
+                }
+            }
+            previousColor = currentColor;
         }
     }
 }
