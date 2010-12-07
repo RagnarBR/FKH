79808c5f16dc6b1d5251e9968456e34164c60d55
SuprDewd <suprdewd@gmail.com>
2010-12-07 00:01:59 +0000

    PMT update.

--- RBR/PimpMyText/PimpMyText/Form1.cs
+++ RBR/PimpMyText/PimpMyText/Form1.cs
@@ -1,50 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 
 namespace PimpMyText
 {
     public partial class Form1 : Form
     {
-        string[] colorStart = new string[] { @"[color=red]", @"[color=blue]", @"[color=cyan]", @"[color=green]", @"[color=black]", @"[color=pink]", @"[color=yellow]", @"[color=purple]", @"[color=pink]"};
-        string colorEnd = @"[/color]";
-        string temp = null;
-
         public Form1()
         {
             InitializeComponent();
         }
 
+        private const string _Open = "[color={0}]";
+        private const string _Close = "[/color]";
+        private readonly string[] _Colors = new[] { "red", "blue", "cyan", "green", "black", "yellow", "pink", "purple" };
+
         private void buttonPimpMyText_Click(object sender, EventArgs e)
         {
-            temp = null;
-            //temp = textBox1.Text; 
-
-            for (int i = 0; i < textBox1.Text.Length; i++)
-            {
-                if (char.IsLetterOrDigit(textBox1.Text[i])) //r i = 0 
-                {
-                    for (int x = 0; x < textBox1.Text.Length; x++) //x = 0
-                    {
-                        if (x != i) 
-                        {
-                            temp += textBox1.Text[x]; 
-                        }
-
-                        else //x = i
-                        {
-                            temp += colorStart[0] + textBox1.Text[x] + colorEnd; //[color=red] + textBox1.Text[0] + [/color]
-                        }
-                    }
-                }
-            }
-
-            textBox1.Text = temp;
+            Random r = new Random();
+            string s = _Open + "{1}" + _Close;
+            this.txtOutput.Text = String.Join("", this.txtInput.Text.Select(c => String.Format(s, this._Colors[r.Next(0, this._Colors.Length)], c)).ToArray());
         }
     }
 }
