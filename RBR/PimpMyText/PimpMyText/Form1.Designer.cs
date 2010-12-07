12ea555db05d22ee5505a771d756fbb4f3fe1ee1
SuprDewd <suprdewd@gmail.com>
2010-12-07 00:18:45 +0000

    PMT update.

--- RBR/PimpMyText/PimpMyText/Form1.Designer.cs
+++ RBR/PimpMyText/PimpMyText/Form1.Designer.cs
@@ -32,35 +32,35 @@
             this.btnPimpMyText = new System.Windows.Forms.Button();
             this.txtOutput = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
+            //
             // txtInput
-            // 
+            //
             this.txtInput.Location = new System.Drawing.Point(12, 12);
             this.txtInput.Multiline = true;
             this.txtInput.Name = "txtInput";
             this.txtInput.Size = new System.Drawing.Size(250, 191);
             this.txtInput.TabIndex = 0;
-            // 
+            //
             // btnPimpMyText
-            // 
+            //
             this.btnPimpMyText.Location = new System.Drawing.Point(275, 96);
             this.btnPimpMyText.Name = "btnPimpMyText";
             this.btnPimpMyText.Size = new System.Drawing.Size(92, 23);
             this.btnPimpMyText.TabIndex = 1;
             this.btnPimpMyText.Text = "Pimp My Text";
             this.btnPimpMyText.UseVisualStyleBackColor = true;
-            this.btnPimpMyText.Click += new System.EventHandler(this.buttonPimpMyText_Click);
-            // 
+            this.btnPimpMyText.Click += new System.EventHandler(this.ButtonPimpMyTextClick);
+            //
             // txtOutput
-            // 
+            //
             this.txtOutput.Location = new System.Drawing.Point(378, 12);
             this.txtOutput.Multiline = true;
             this.txtOutput.Name = "txtOutput";
             this.txtOutput.Size = new System.Drawing.Size(242, 191);
             this.txtOutput.TabIndex = 0;
-            // 
+            //
             // Form1
-            // 
+            //
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(632, 219);
@@ -71,14 +71,12 @@
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
 
-        #endregion
+        #endregion Windows Form Designer generated code
 
         private System.Windows.Forms.TextBox txtInput;
         private System.Windows.Forms.Button btnPimpMyText;
         private System.Windows.Forms.TextBox txtOutput;
     }
 }
-
--- RBR/PimpMyText/PimpMyText/Form1.cs
+++ RBR/PimpMyText/PimpMyText/Form1.cs
@@ -14,12 +14,27 @@ namespace PimpMyText
         private const string _Open = "[color={0}]";
         private const string _Close = "[/color]";
         private readonly string[] _Colors = new[] { "red", "blue", "cyan", "green", "black", "yellow", "pink", "purple" };
+        private string _LastColor;
+        private readonly Random Rand = new Random();
 
-        private void buttonPimpMyText_Click(object sender, EventArgs e)
+        private void ButtonPimpMyTextClick(object sender, EventArgs e)
         {
-            Random r = new Random();
-            string s = _Open + "{1}" + _Close;
-            this.txtOutput.Text = String.Join("", this.txtInput.Text.Select(c => String.Format(s, this._Colors[r.Next(0, this._Colors.Length)], c)).ToArray());
+            const string s = _Open + "{1}" + _Close;
+            this.txtOutput.Text = String.Join("", this.txtInput.Text.Select(c => String.Format(s, NewColor(), c)).ToArray());
+        }
+
+        private string NewColor()
+        {
+            string n;
+
+            do
+            {
+                n = this._Colors[Rand.Next(0, this._Colors.Length)];
+            }
+            while (n == _LastColor);
+
+            _LastColor = n;
+            return n;
         }
     }
 }
