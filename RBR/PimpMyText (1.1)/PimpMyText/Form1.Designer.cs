ea3b971fbe2d96d141c8896142b0de808ed37d0e
RagnarBR <johnnyahss@hotmail.com>
2010-12-09 18:35:23 +0000

    1.1

---
+++ RBR/PimpMyText (1.1)/PimpMyText/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace PimpMyText
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonPimpMyText = new System.Windows.Forms.Button();
+            this.buttonMorePimpin = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(268, 191);
+            this.textBox1.TabIndex = 0;
+            // 
+            // buttonPimpMyText
+            // 
+            this.buttonPimpMyText.Location = new System.Drawing.Point(12, 209);
+            this.buttonPimpMyText.Name = "buttonPimpMyText";
+            this.buttonPimpMyText.Size = new System.Drawing.Size(85, 23);
+            this.buttonPimpMyText.TabIndex = 1;
+            this.buttonPimpMyText.Text = "Pimp my text!";
+            this.buttonPimpMyText.UseVisualStyleBackColor = true;
+            this.buttonPimpMyText.Click += new System.EventHandler(this.buttonPimpMyText_Click);
+            // 
+            // buttonMorePimpin
+            // 
+            this.buttonMorePimpin.Enabled = false;
+            this.buttonMorePimpin.Location = new System.Drawing.Point(12, 238);
+            this.buttonMorePimpin.Name = "buttonMorePimpin";
+            this.buttonMorePimpin.Size = new System.Drawing.Size(122, 23);
+            this.buttonMorePimpin.TabIndex = 2;
+            this.buttonMorePimpin.Text = "Do some more pimpin\'";
+            this.buttonMorePimpin.UseVisualStyleBackColor = true;
+            this.buttonMorePimpin.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "After each character",
+            "After each sentence",
+            "After each word",
+            "Custom"});
+            this.comboBox1.Location = new System.Drawing.Point(159, 238);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(185, 214);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Change color";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 272);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.buttonMorePimpin);
+            this.Controls.Add(this.buttonPimpMyText);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Pimp my text ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonPimpMyText;
+        private System.Windows.Forms.Button buttonMorePimpin;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ RBR/PimpMyText (1.1)/PimpMyText/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PimpMyText
+{
+    public partial class Form1 : Form
+    {
+        string[] colorStart = new string[] { @"[color=red]", @"[color=blue]", @"[color=cyan]", @"[color=green]", @"[color=black]", @"[color=pink]", @"[color=yellow]", @"[color=purple]"};
+        string colorEnd = @"[/color]";
+        string temp = null;
+        Random rand = new Random();
+        int currentColor = 0, previousColor = 100;
+        bool itemSelected = false, newWord = false;
+
+        //Breytti DropDownStyle úr Dropdown í DropDownList til þess að disablea writing í því
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonPimpMyText_Click(object sender, EventArgs e)
+        {
+            itemSelected = false;
+
+            if (comboBox1.Text == "After each character")
+            {
+                temp = null;
+                itemSelected = true;
+                for (int i = 0; i < textBox1.Text.Length; i++)
+                {
+                    if (!char.IsWhiteSpace(textBox1.Text[i]))
+                    {
+                        for (int x = 0; x < textBox1.Text.Length; x++) //x = 0
+                        {
+                            if (x == i) //x = i
+                            {
+                                NextColor();
+                                temp += colorStart[currentColor] + textBox1.Text[x] + colorEnd; //[color=red] + textBox1.Text[0] + [/color]
+                            }
+                        }
+                    }
+
+                    else if (textBox1.Text[i] == ' ')
+                    {
+                        temp += ' ';
+                    }
+
+                    else if (textBox1.Text[i] == '\t')
+                    {
+                        temp += "\t";
+                    }
+
+                    else if (textBox1.Text[i] == '\n')
+                    {
+                        temp += '\n';
+                    }
+                }
+            }
+
+            else if (comboBox1.Text == "After each word")
+            {
+                temp = null;
+                itemSelected = true;
+
+                for (int i = 0; i < textBox1.Text.Length; i++)
+                {
+                    NextColor();
+                    #region FortheFirstWord
+                    if (i == 0) //Set color tag fyrir framan fyrsta orðið
+                    {
+                        temp += colorStart[currentColor];
+                    }
+                    #endregion 
+
+                    if (!char.IsWhiteSpace(textBox1.Text[i]))//textBox1.Text[i] != ' ')
+                    {
+                        if (newWord)
+                        {
+                            temp += colorStart[currentColor];
+                            newWord = false;
+                        }
+                        temp += textBox1.Text[i];
+                    }
+
+                    else if (textBox1.Text[i] == ' ')
+                    {
+                        temp += colorEnd; 
+                        temp += ' ';
+                        newWord = true;
+                    }
+
+                    else if (textBox1.Text[i] == '\t')
+                    {
+                        temp += colorEnd;
+                        temp += "\t";
+                        newWord = true; 
+                    }
+
+                    else if (textBox1.Text[i] == '\n')
+                    {
+                        temp += colorEnd; 
+                        temp += '\n';
+                        newWord = true; 
+                    }
+
+                  /*  else
+                    {
+                        temp += colorEnd; 
+                        temp += ' ';
+                        newWord = true;
+                    }*/ 
+
+                    #region FortheLastWord
+                    if (i == (textBox1.Text.Length - 1))
+                    {
+                        temp += colorEnd;
+                    }
+                    #endregion 
+                }
+
+                textBox1.Text = temp;
+            }
+
+            else
+            {
+                MessageBox.Show("Please pick an item from the combobox.");
+            } 
+
+            
+
+            if (itemSelected)
+            {
+                textBox1.Text = temp;
+                textBox1.ReadOnly = true;
+                buttonMorePimpin.Enabled = true;
+                comboBox1.Enabled = false;
+                buttonPimpMyText.Enabled = false;
+            }
+        }
+
+        public void NextColor()
+        {
+            bool useColor = false;
+
+            while (useColor == false)
+            {
+                currentColor = rand.Next(0, (colorStart.Length - 1));
+
+                if (currentColor == previousColor)
+                {
+                    useColor = false;
+                }
+
+                else
+                {
+                    useColor = true;
+                }
+            }
+            previousColor = currentColor;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox1.ReadOnly = false;
+            buttonMorePimpin.Enabled = false;
+            comboBox1.Enabled = true;
+            buttonPimpMyText.Enabled = true;
+        }
+    }
+}
